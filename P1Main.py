d032c5933bd533f39928a25706022b070bf9e7ac
Hullaballo2001 <chezroulietta@gmail.com>
2021-10-18 15:56:44 +0200

    ajout des fichiers python

---
+++ P1Main.py
@@ -0,0 +1,11 @@
+import os
+# On va dire qu'on commence à lire/chiffrer depuis C:\users
+
+# CONSTRUCTION DU CHEMIN DYNAMIQUE
+#---------------------------------
+
+# récupération lettre DD principal
+
+#récupération du nom du user
+user_name = os.getlogin()
+
